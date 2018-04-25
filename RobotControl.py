a82b67007936cd2d3b551b3280d983bb763e25dd
Jake Erichson <jake@Jake-Erichsons-MacBook.local>
2018-04-25 17:39:17 -0400

    Cal.15

--- RobotControl.py
+++ RobotControl.py
@@ -307,9 +307,8 @@ def start():
 
         # hardcoded rotation and yaw counter with x-axis D-pad
         elif not gamepad_Dx == 0:
-            print(gamepad_Dx)
-            print("-")
-            time.sleep(1)
+            chassisMove(gamepad_Dx*127.5, 0)
+            yaw_actuation(-1*gamepad_Dx*127.5)
 
     cleanup(int(angleTracker / (1.8 / 2.4)))
 
